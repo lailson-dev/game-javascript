918b67d619bf7d01c891a96acf90494e70af0845
Lailson Conceição <Lailsoncv@hotmail.com>
2017-06-13 19:23:47 -0300

    Finalizando o jogo

--- game.js
+++ game.js
@@ -35,3 +35,101 @@ monsterImage.onload = function () {
 	monsterReady = true;
 };
 monsterImage.src = 'images/monster.png';
+
+// Objetos do jogo 
+var hero = {
+	speed: 256 // movimento em pixels por segundo
+};
+
+var monster = {};
+var monstersCaught = 0; // Contador dos monstros pegos
+
+// Controle de teclado
+var keysDown = {};
+
+window.addEventListener('keydown', function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+window.addEventListener('keyup', function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reseta o jogo quando o jogador pega o monstro
+var reset = function reset() {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Posiciona o monstro randomicamente na tela
+	monster.x = 32 + Math.random() * (canvas.width - 64);
+	monster.y = 32 + Math.random() * (canvas.height - 64);
+};
+
+// Atualiza os objetos do jogo
+var update = function update(modifier) {
+	if (38 in keysDown) {
+		// pressionando a seta para cima
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) {
+		// pressionando a seta para baixo
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) {
+		// pressionando a seta para a esquerda
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) {
+		// Pressionando a seta para a direita
+		hero.x += hero.speed * modifier;
+	}
+
+	// Os personagens se encostaram?
+	if (hero.x <= monster.x + 32 && monster.x <= hero.x + 32 && hero.y <= monster.y + 32 && monster.y <= hero.y + 32) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+// Renderização
+var render = function render() {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Pontuação
+	ctx.fillStyle = 'rgb(250, 250, 250)';
+	ctx.font = '24px Helvetica';
+	ctx.textAlign = 'left';
+	ctx.textBaseline = 'top';
+	ctx.fillText('Monstros pegos: ' + monstersCaught, 32, 32);
+};
+
+// Controla o loop do jogo
+var main = function main() {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+	requestAnimationFrame(main);
+};
+
+// Suporte cross-browser para requestAnimationFrame
+var w = window;
+var requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+//Que comece o jogo!
+var then = Date.now();
+reset();
+main();
--- gameBabel.js
+++ gameBabel.js
@@ -33,3 +33,103 @@ monsterImage.onload = function() {
 	monsterReady = true;
 };
 monsterImage.src = 'images/monster.png';
+
+// Objetos do jogo 
+const hero = {
+	speed: 256 // movimento em pixels por segundo
+};
+
+const monster = {};
+let monstersCaught = 0; // Contador dos monstros pegos
+
+// Controle de teclado
+const keysDown = {};
+
+window.addEventListener('keydown', function(e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+window.addEventListener('keyup', function(e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reseta o jogo quando o jogador pega o monstro
+const reset = function() {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Posiciona o monstro randomicamente na tela
+	monster.x = 32 + (Math.random() *(canvas.width - 64));
+	monster.y = 32 + (Math.random() *(canvas.height - 64));
+};
+
+// Atualiza os objetos do jogo
+const update = function(modifier) {	
+	if(38 in keysDown) { // pressionando a seta para cima
+		hero.y -= hero.speed * modifier;
+	}
+	if(40 in keysDown) { // pressionando a seta para baixo
+		hero.y += hero.speed * modifier;
+	}
+	if(37 in keysDown) { // pressionando a seta para a esquerda
+		hero.x -= hero.speed * modifier; 
+	}
+	if(39 in keysDown) { // Pressionando a seta para a direita
+		hero.x += hero.speed * modifier;
+	}
+
+	// Os personagens se encostaram?
+	if(
+		hero.x <= (monster.x + 32) &&
+		monster.x <= (hero.x + 32) &&
+		hero.y <= (monster.y + 32) &&
+		monster.y <= (hero.y + 32)
+
+		) {
+			++monstersCaught;
+			reset();
+	}
+};
+
+// Renderização
+const render = function() {
+	if(bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if(heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if(monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Pontuação
+	ctx.fillStyle = 'rgb(250, 250, 250)';
+	ctx.font = '24px Helvetica';
+	ctx.textAlign = 'left';
+	ctx.textBaseline = 'top';
+	ctx.fillText('Monstros pegos: ' + monstersCaught, 32, 32);
+};
+
+// Controla o loop do jogo
+const main = function() {
+	const now = Date.now();
+	const delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+	requestAnimationFrame(main);
+};
+
+// Suporte cross-browser para requestAnimationFrame
+const w = window;
+const requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+//Que comece o jogo!
+let then = Date.now();
+reset();
+main();
